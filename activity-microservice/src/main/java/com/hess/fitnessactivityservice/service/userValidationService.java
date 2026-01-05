dd6b9eda2cd30a54b66e1ac2455473b3f076e736
Hesanda Liyanage <130324291+HesandaLiyanage@users.noreply.github.com>
2026-01-05 20:18:50 +0530

    issues fixed.

--- activity-microservice/src/main/java/com/hess/fitnessactivityservice/service/userValidationService.java
+++ activity-microservice/src/main/java/com/hess/fitnessactivityservice/service/userValidationService.java
@@ -17,7 +17,7 @@ public class userValidationService {
         try {
             return Boolean.TRUE.equals(userServiceWebClient.get().uri("/api/users/{userId}/validate", userId)
                     .retrieve()
-                    .bodyToMono(Boolean.class));
+                    .bodyToMono(Boolean.class).block());
 
         } catch (WebClientResponseException e) {
             if(e.getStatusCode() == HttpStatus.NOT_FOUND)
